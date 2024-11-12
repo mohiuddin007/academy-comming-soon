17140b3e01d183cd2fdac8fed06b516001aab9e7
mohiuddin007 <mohiuddinmazumder94@gmail.com>
2024-11-12 21:15:59 +0600

    background blur

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,10 +13,11 @@ export default function Home() {
           layout="responsive" // Makes the image responsive
           width={1080} // Set the width and height based on your image's aspect ratio
           height={1920}
-          priority={true} // Prioritize loading
+          priority={true}
+          className="filter blur-sm"
         />
       </div>
-      <div className="fixed inset-0 flex justify-center pt-[180px] z-50">
+      <div className="fixed inset-0 flex justify-center pt-[180px] z-50 bg-black bg-opacity-50">
         <div className="bg-white h-[500px] w-[90%] md:w-[60%] p-6 bg-opacity-90 rounded-lg shadow-lg mx-4 rounded-xl">
           <div className="flex items-center justify-center mt-[50px]">
             <Image
